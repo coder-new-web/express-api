8ebca83997759dd40af24705ec21e8e53260adb2
coder-new <2578417052@qq.com>
2023-03-28 14:00:13 +0800

    config🔧: update index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express, { Express } from "express";
-import randomGraph from "./router/random-graph";
+import randomGraphRouter from "./router/random-graph";
 
 const app: Express = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.use(randomGraph);
+app.use(randomGraphRouter);
 
 app.listen(5153, () => {
   console.log("Server run on http://localhost:5153");
